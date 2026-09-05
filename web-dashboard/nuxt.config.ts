e2dda20405eb3bde270683a8e8d540e53c1a5c79
raditya <radityadamargalih12@gmail.com>
2026-09-05 15:49:34 +0700

    q

--- web-dashboard/nuxt.config.ts
+++ web-dashboard/nuxt.config.ts
@@ -1,3 +1,36 @@
+// Polyfill Set methods for compatibility with Node 20 / older environments
+if (typeof Set !== 'undefined') {
+  if (!Set.prototype.difference) {
+    Set.prototype.difference = function (other: Set<any>) {
+      const result = new Set(this)
+      for (const elem of other) {
+        result.delete(elem)
+      }
+      return result
+    }
+  }
+  if (!Set.prototype.union) {
+    Set.prototype.union = function (other: Set<any>) {
+      const result = new Set(this)
+      for (const elem of other) {
+        result.add(elem)
+      }
+      return result
+    }
+  }
+  if (!Set.prototype.intersection) {
+    Set.prototype.intersection = function (other: Set<any>) {
+      const result = new Set()
+      for (const elem of other) {
+        if (this.has(elem)) {
+          result.add(elem)
+        }
+      }
+      return result
+    }
+  }
+}
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
