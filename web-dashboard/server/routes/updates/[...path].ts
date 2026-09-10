6b60e0e14f45d77fcf600042218ee1b2c87a80ec
raditya <radityadamargalih12@gmail.com>
2026-09-10 12:24:40 +0700

    ajsnjabdc

---
+++ web-dashboard/server/routes/updates/[...path].ts
@@ -0,0 +1,27 @@
+export default defineEventHandler(async (event) => {
+  const config = useRuntimeConfig()
+  const backendBase = (process.env.BACKEND_URL || config.backendUrl || 'http://127.0.0.1:10002').replace(/\/+$/, '')
+  const subPath = event.context.params?.path || ''
+  const targetUrl = `${backendBase}/updates/${subPath}`
+
+  try {
+    const response = await fetch(targetUrl, {
+      method: event.method,
+      headers: { 'Accept': '*/*' }
+    })
+
+    setResponseStatus(event, response.status)
+    response.headers.forEach((val, key) => {
+      if (key !== 'transfer-encoding' && key !== 'content-encoding') {
+        setHeader(event, key, val)
+      }
+    })
+
+    const data = await response.arrayBuffer()
+    return Buffer.from(data)
+  } catch (err: any) {
+    console.error(`[Updates Proxy Error to ${targetUrl}]:`, err.message)
+    setResponseStatus(event, 502)
+    return { success: false, error: err.message }
+  }
+})
